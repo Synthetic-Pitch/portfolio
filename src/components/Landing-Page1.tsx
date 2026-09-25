d77eb436536eccfe16445c401e204c9fa09f0f41
reymark <genuinepower123@gmail.com>
2026-09-25 20:39:57 +0800

    main

--- src/components/Landing-Page1.tsx
+++ src/components/Landing-Page1.tsx
@@ -119,8 +119,8 @@ const LandingPage1 = ({ parentCon }: LandingPage1Props) => {
         </div>
       </section>
       <section ref={section3nd} className="absolute top-0 h-dvh w-full flex justify-end items-end z-10">
-        <div className="absolute bottom-0 h-[24] w-[16rem] flex items-center justify-center">
-          <img src={kingspade} alt="" className="object-contain"/>
+        <div className="absolute bottom-0 h-89 w-[16rem] flex items-center justify-center bg-[red] m-4">
+          
         </div>
       </section>
     </div>
